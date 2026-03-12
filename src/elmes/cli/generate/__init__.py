5066eed9aceb79b3b49a13040d59f5eeadc0ad38
Masr160 <wapollo9@gmail.com>
2026-03-12 19:00:39 +0800

    ♻️ refactor(generate): remove langchain debug dependency

--- src/elmes/cli/generate/__init__.py
+++ src/elmes/cli/generate/__init__.py
@@ -1,16 +1,12 @@
 import click
 
 from pathlib import Path
-from langchain.globals import set_debug
-
-# set_debug(True)
 
 
 @click.command("generate", help="Generate conversions for all tasks")
 @click.option("--config", default="config.yaml", help="Path to the configuration file.")
 @click.option("--debug", default=False, help="Debug Mode", is_flag=True)
 def generate(config: str, debug: bool):
-    set_debug(debug)
     from elmes.config import load_conf
 
     path = Path(config)
