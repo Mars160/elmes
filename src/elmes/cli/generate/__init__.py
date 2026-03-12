b596ac38c60ff21ed98e7cce09186adc475f392e
Masr160 <wapollo9@gmail.com>
2026-03-13 01:03:48 +0800

    ✨ feat(generate): implement full generate logic with task cloning

--- src/elmes/cli/generate/__init__.py
+++ src/elmes/cli/generate/__init__.py
@@ -1,6 +1,12 @@
 from elmes.config import ElmesConfig
+from elmes.agent import Agent
+from elmes.graph import Graph
+from elmes.client import Client
+
+from tqdm.asyncio import tqdm
 
 import click
+import asyncio
 
 from pathlib import Path
 
@@ -17,7 +23,35 @@ def generate(config: str, debug: bool):
 
 
 def generate_logic(config: ElmesConfig, debug: bool):
-    from elmes.run import run
-    import asyncio
+    clients = {}
+    for model_name, model_config in config.models.items():
+        clients[model_name] = Client.from_model_config(
+            model_config, config.globals.retry
+        )
 
-    asyncio.run(run())
+    agents = {}
+    for agent_name, agent_config in config.agents.items():
+        model_name = agent_config.model
+        if model_name not in clients:
+            raise ValueError(f"Model {model_name} not found for agent {agent_name}")
+        client = clients[model_name]
+        agents[agent_name] = Agent(
+            name=agent_name,
+            agent_config=agent_config,
+            memory_config=config.globals.memory,
+            client=client,
+        )
+
+    graph, router_node_map = Graph.from_direction_config(config.directions)
+    graphs = []
+    # 根据task数量复制图，并替换节点
+    for task_id, task in enumerate(config.tasks.variables):
+        cloned_graph = graph.clone()
+        graph_agents = {}
+        for agent in agents:
+            graph_agents[agent] = agents[agent].clone(
+                task_variables=task, task_id=task_id
+            )
+        cloned_graph.replace_node({**graph_agents, **router_node_map})
+        graphs.append(cloned_graph)
+        # TODO: 实现tasks.start_prompt的替换
